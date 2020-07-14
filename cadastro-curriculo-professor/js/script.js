10732911521e6bc73b28fcd8decb0e312033b675
Jardel <developerprojectfullstack@gmail.com>
2020-07-14 19:28:36 -0300

    comentário adicionado no js

--- cadastro-curriculo-professor/js/script.js
+++ cadastro-curriculo-professor/js/script.js
@@ -1,4 +1,6 @@
+/* Initial Console */
 console.log('Hello World!');
 
+/* Change the 'H1' Curriculum Title */
 var title = document.querySelector('h1');
 title.textContent = 'O título do Raphael Gomes foi modificado por Jardel Cunha';
