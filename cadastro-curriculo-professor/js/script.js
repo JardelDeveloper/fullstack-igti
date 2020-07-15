7f544b26d4222f21d7d966dbf08dbb1a6781427d
Jardel <developerprojectfullstack@gmail.com>
2020-07-15 12:51:01 -0300

    aplicando js com switch e if-else

--- cadastro-curriculo-professor/js/script.js
+++ cadastro-curriculo-professor/js/script.js
@@ -26,3 +26,53 @@ var d = 5;
 if (c > d) console.log(c + ' é maior que ' + d);
 else if (c < d) console.log(c + ' é menor que ' + d);
 else console.log(c + ' é igual a ' + d);
+
+/* if-else example */
+var dia = 2;
+if (dia === 1) {
+  console.log('Domingo');
+} else {
+  if (dia === 2) {
+    console.log('Segunda');
+  } else {
+    if (dia === 3) {
+      console.log('Terça');
+    } else {
+      if (dia === 4) {
+      } else {
+        if (dia === 5) {
+          console.log('Quinta');
+        } else {
+          if (dia === 6) {
+            console.log('Sexta');
+          } else {
+            if (dia === 7) {
+              console.log('Sábado');
+            } else {
+              console.log('Dia inválido');
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+/* switch example */
+var dia = 11;
+var r;
+
+// prettier-ignore
+switch (dia) {
+  case 1: r = 'Domingo'; break;
+  case 2: r = 'Segunda'; break;
+  case 3: r = 'Terça'; break;
+  case 4: r = 'Quarta'; break;
+  case 5: r = 'Quinta'; break;
+  case 6: r = 'Sexta'; break;
+  case 7: r = 'Sábado'; break;
+  
+  default: r = 'Dia inválido';
+}
+
+console.log(r);
