b6b38dc64b94750fb3b2b97e2c606deafb59a234
Jardel <developerprojectfullstack@gmail.com>
2020-07-16 19:25:06 -0300

    manipulando css no javascript

--- curriculo-professor/js/dom.js
+++ curriculo-professor/js/dom.js
@@ -11,7 +11,7 @@ var personalData = document.querySelectorAll('li');
 personalData = Array.from(personalData);
 console.log(personalData);
 
-/* Summary of Query Selector All  with Array.From */
+/* Summary of Query Selector All with Array.From */
 var div = Array.from(document.querySelectorAll('div'));
 console.log(div);
 
@@ -22,7 +22,12 @@ for (var i = 0; i < li.length; i++) {
   teste.style.color = 'blue';
 }
 
+/* Change class of li with for getting "var li" before */
 for (var i = 0; i < li.length; i++) {
   var currentElement = li[i];
   currentElement.classList.add('emphasis');
 }
+
+/* Add class of table */
+var h1 = document.querySelector('table');
+h1.classList.add('table-custom');
