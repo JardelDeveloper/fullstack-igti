61ab2ece39737c3eca945b6df54017bf318a8c43
Jardel <developerprojectfullstack@gmail.com>
2020-07-17 14:43:53 -0300

    estilizando form

--- form-crud/js/scripts.js
+++ form-crud/js/scripts.js
@@ -1,3 +1,9 @@
+// Initializing correctly the page
 window.addEventListener('load', start);
 
-function start() {}
+// Definition of a global variable
+var globalNames = ['Um', 'Dois', 'Três', 'Quatro'];
+
+function start() {
+  console.log('start');
+}
