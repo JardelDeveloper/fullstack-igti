1e4e93c56a4cb23d676b3285584c8ad5b2cb87ac
Jardel <developerprojectfullstack@gmail.com>
2020-07-20 19:42:55 -0300

    Manipulando arrays em E6+ com map

--- es6+/manipulating-arrays/js/script.js
+++ es6+/manipulating-arrays/js/script.js
@@ -1,1 +1,9 @@
-console.log(people);
+window.addEventListener('load', () => {
+  const nameEmailArray = people.map((person) => {
+    return {
+      name: person.name,
+      email: person.email,
+    };
+  });
+});
+// console.log(people);
