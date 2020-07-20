49fe647fd6135779673be0e6b1b46958781865a4
Jardel <developerprojectfullstack@gmail.com>
2020-07-20 11:30:02 -0300

    calculo de idade manipulando dom

--- practical-activity/js/scripts.js
+++ practical-activity/js/scripts.js
@@ -1,17 +1,39 @@
 window.addEventListener('load', start);
 
 function start() {
-  // console.log(textYearly);
+  console.log('start');
 
   var inputYear = document.querySelector('#inputYear');
-  inputYear.addEventListener('keyup', countName);
+  var buttonCalc = document.querySelector('#buttonCalc');
+  inputYear.addEventListener('click', showYear);
+  buttonCalc.addEventListener('click', calc);
+
+  activateInput();
 }
 
-function countName() {
+function showYear() {
   var textYearly = document.querySelector('#textYearly');
   var date = new Date();
   var yearly = date.getFullYear();
   var yearly = new Date().getFullYear();
 
   textYearly.textContent = 'Ano atual: ' + yearly;
+  console.log(yearly);
+
+  function handleYear(event) {
+    var total = event.target.value;
+    teste = yearly - total;
+  }
+
+  inputYear.addEventListener('keyup', handleYear);
+}
+
+function calc() {
+  alert(inputName.value + ' , você tem ' + teste + ' anos');
+  showYear();
+  handleYear();
+}
+
+function activateInput() {
+  inputName.focus();
 }
