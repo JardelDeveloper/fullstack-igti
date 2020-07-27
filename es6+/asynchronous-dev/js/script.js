099fd0e30479e283c2348fe87a03cc3c7b4a840d
Jardel <developerprojectfullstack@gmail.com>
2020-07-27 17:43:42 -0300

    counter that displays numbers with setTimeout, setInterval and clearInterval

--- es6+/asynchronous-dev/js/script.js
+++ es6+/asynchronous-dev/js/script.js
@@ -1,1 +1,21 @@
+// counter that displays numbers with setTimeout, setInterval and clearInterval
 console.log('start');
+window.addEventListener('load', function () {
+  const timer = document.querySelector('#timer');
+  let count = 0;
+
+  const interval = setInterval(() => {
+    timer.textContent = ++count;
+
+    if (count === 10) {
+      this.clearInterval(interval);
+      return;
+    }
+
+    if (count % 5 === 0) {
+      setTimeout(() => {
+        timer.textContent = count + ',5';
+      }, 500);
+    }
+  }, 1000);
+});
