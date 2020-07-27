20c79c2bb142ac68862e3f962328e289fc195396
Jardel <developerprojectfullstack@gmail.com>
2020-07-27 19:41:38 -0300

    js com fetch

--- es6+/fetch/js/script.js
+++ es6+/fetch/js/script.js
@@ -1,1 +1,16 @@
-console.log('fetch');
+window.addEventListener('load', function () {
+  fetch('https://api.github.com/users/rrgomide').then((res) => {
+    // console.log(res);
+    res.json().then((data) => {
+      showData(data);
+    });
+  });
+});
+
+function showData(data) {
+  const userId = document.querySelector('#userId');
+  const userName = document.querySelector('#userName');
+  userId.textContent = 'id do usuário: ' + data.id;
+  userName.textContent = 'nome do usuário: ' + data.name;
+  console.log(data);
+}
